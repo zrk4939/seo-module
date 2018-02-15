674065e91686d974b102a2ec5d545ce9dfb6fafc
zrk4939 <zrk4939@gmail.com>
2018-02-15 13:01:03 +0300

    toggle form js

--- widgets/assets/dist/js/metaWidget.js
+++ widgets/assets/dist/js/metaWidget.js
@@ -6,10 +6,16 @@ $(document).ready(function () {
     $('.manual-meta').hide();
 
     $('#seo-manual').on('change', function () {
-        if ($(this).is(":checked")) {
+        toggleForm($(this));
+    });
+
+    var toggleForm = function ($el) {
+        if ($el.is(":checked")) {
             $('.manual-meta').show();
             return;
         }
         $('.manual-meta').hide();
-    });
+    }
+
+    toggleForm($('#seo-manual'));
 });
